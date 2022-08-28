f83e7be2deda8d76004eca22415bbf18bc21cf83
Wang <84210770+Wang-sus@users.noreply.github.com>
2022-08-28 11:03:03 -0400

    Create main.js

---
+++ client/API/main.js
@@ -0,0 +1,5 @@
+const API = {
+  get game() { return document.getElementById('game').contentWindow },
+  get lesson() { return document.getElementById('html5Iframe').contentWindow },
+  get learningGame() { return document.getElementById('learning-game-frame').contentWindow }
+}
